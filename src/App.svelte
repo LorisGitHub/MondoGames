348bb5af726a0ad07d4ba564e037e14f88793ba6
loris <loris.du.84@outlook.fr>
2020-10-14 12:34:17 +0200

    first routes and overall layout

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,33 @@
 <script lang="ts">
 	let name = 'world';
+    import routes from './routes'
+    import Router from 'svelte-spa-router'
+    import TopAppBar, {Row, Section, Title} from '@smui/top-app-bar'
+    import IconButton from '@smui/icon-button'
 </script>
 
-<h1>Hello {name}!</h1>
+<div class="top-app-bar-container flexor">
+    <TopAppBar variant="static" color='primary'>
+        <Row>
+            <Section>
+                <IconButton class="material-icons m-0">menu</IconButton>
+                <Title>Flex Static</Title>
+            </Section>
+            <Section align="end" toolbar>
+                <IconButton class="material-icons m-0" aria-label="Download">file_download</IconButton>
+                <IconButton class="material-icons m-0" aria-label="Print this page">print</IconButton>
+                <IconButton class="material-icons m-0" aria-label="Bookmark this page">bookmark</IconButton>
+            </Section>
+        </Row>
+    </TopAppBar>
+    <div class="flexor-content">
+        <Router {routes} />
+    </div>
+    <ul class="app-navbar">
+        <li><a href="#/"><IconButton class="material-icons white m-0">people</IconButton></a></li>
+        <li><a href="#/hello/svelte"><IconButton class="material-icons white m-0">T</IconButton></a></li>
+        <li><a href="#/wild/card"><IconButton class="material-icons white m-0">home</IconButton></a></li>
+        <li><a href="#/does/not/exist"><IconButton class="material-icons white m-0">shopping_cart</IconButton></a></li>
+        <li><a href="#/hello/svelte"><IconButton class="material-icons white m-0">map</IconButton></a></li>
+    </ul>
+</div>
---
+++ src/routes.js
@@ -0,0 +1,22 @@
+// Components
+import Home from './components/Home.svelte'
+import Name from './components/Name.svelte'
+import Wild from './components/Wild.svelte'
+import NotFound from './components/NotFound.svelte'
+
+// Export the route definition object
+export default {
+    // Exact path
+    '/': Home,
+
+    // Using named parameters, with last being optional
+    '/hello/:first/:last?': Name,
+
+    // Wildcard parameter
+    // Included twice to match both `/wild` (and nothing after) and `/wild/*` (with anything after)
+    '/wild': Wild,
+    '/wild/*': Wild,
+
+    // Catch-all, must be last
+    '*': NotFound,
+}
