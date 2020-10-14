43655c9f44788d460d63ed450c8aa8bb3f7fecd2
loris <loris.du.84@outlook.fr>
2020-10-14 12:46:28 +0200

    drawer

--- src/App.svelte
+++ src/App.svelte
@@ -4,18 +4,51 @@
     import Router from 'svelte-spa-router'
     import TopAppBar, {Row, Section, Title} from '@smui/top-app-bar'
     import IconButton from '@smui/icon-button'
+    import Drawer, {AppContent, Content, Header, Subtitle, Scrim} from '@smui/drawer';
+    import List, {Item, Text, Graphic, Separator, Subheader} from '@smui/list';
+    let myDrawer;
+    let myDrawerOpen = false;
+    let active = 'Gray Kittens';
+
+    function setActive(value) {
+        active = value;
+        myDrawerOpen = false;
+    }
 </script>
 
 <div class="top-app-bar-container flexor">
+    <Drawer variant="dismissible" style="width: 80%" bind:this={myDrawer} bind:open={myDrawerOpen}>
+        <Header>
+            <Title>Super Drawer</Title>
+        </Header>
+        <Content>
+            <List>
+                <Item href="javascript:void(0)" on:click={() => setActive('Gray Kittens')} activated={active === 'Gray Kittens'}>
+                    <Text>Gray Kittens</Text>
+                </Item>
+                <Item href="javascript:void(0)" on:click={() => setActive('A Space Rocket')} activated={active === 'A Space Rocket'}>
+                    <Text>A Space Rocket</Text>
+                </Item>
+                <Item href="javascript:void(0)" on:click={() => setActive('100 Pounds of Gravel')} activated={active === '100 Pounds of Gravel'}>
+                    <Text>100 Pounds of Gravel</Text>
+                </Item>
+                <Item href="javascript:void(0)" on:click={() => setActive('All of the Shrimp')} activated={active === 'All of the Shrimp'}>
+                    <Text>All of the Shrimp</Text>
+                </Item>
+                <Item href="javascript:void(0)" on:click={() => setActive('A Planet with a Mall')} activated={active === 'A Planet with a Mall'}>
+                    <Text>A Planet with a Mall</Text>
+                </Item>
+            </List>
+        </Content>
+    </Drawer>
     <TopAppBar variant="static" color='primary'>
         <Row>
             <Section>
-                <IconButton class="material-icons m-0">menu</IconButton>
-                <Title>Flex Static</Title>
+                <IconButton on:click={() => myDrawerOpen = !myDrawerOpen} class="material-icons m-0">menu</IconButton>
+                <Title>Mondo Games</Title>
             </Section>
             <Section align="end" toolbar>
-                <IconButton class="material-icons m-0" aria-label="Download">file_download</IconButton>
-                <IconButton class="material-icons m-0" aria-label="Print this page">print</IconButton>
+                <IconButton class="material-icons m-0" aria-label="Download">search</IconButton>
                 <IconButton class="material-icons m-0" aria-label="Bookmark this page">bookmark</IconButton>
             </Section>
         </Row>
