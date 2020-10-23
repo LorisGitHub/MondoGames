5ad33d87f13b0fdd91e4e3589e92d949d7b4d0eb
loris <loris.du.84@outlook.fr>
2020-10-23 16:23:34 +0200

    friends

--- src/components/friends/Friends.svelte
+++ src/components/friends/Friends.svelte
@@ -19,44 +19,22 @@
     }
 </script>
 
-<Content component={List}>
-    {#each [...Array(6)].map((v, i) => i + 1) as item}
-        <Item on:click={() => clicked++}>
+<Content class="horizontal-list" component={List}>
+    {#each [...Array(12)].map((v, i) => i + 1) as item}
+        <div style="padding: 0 10px; text-align: center" on:click={() => clicked++}>
+            <IconButton class="round-border material-icons m-0">people</IconButton>
             <Text>Item #{item}</Text>
-        </Item>
+        </div>
     {/each}
 </Content>
 
 <Card class="w100 mt-10">
-    <Content component={List}>
-        {#each [...Array(6)].map((v, i) => i + 1) as item}
-            <Item on:click={() => clicked++}>
-                <Text>Item #{item}</Text>
+    <Content component={List} class="p-0">
+        {#each [...Array(20)].map((v, i) => i + 1) as item}
+            <Item class="pb-5 pt-5" style="border-bottom: 1px solid" on:click={() => clicked++}>
+                <IconButton class="round-border material-icons m-0">people</IconButton>
+                <Text class="ml-10">Item #{item}</Text>
             </Item>
         {/each}
     </Content>
 </Card>
-
-<!--<h2>Home component</h2>
-
-<p>
-    This sample shows how to set up the router with minimum functionality. <br/>
-    The route definition object contains a number of routes (including some with parameters and a catch-all at the end).<br/>
-    The links below allow navigating between pages.
-</p>
-
-<p>This is the Home component, which contains markup only.</p>
-
-<p><em>Hint:</em> Try navigating with the links below, then use your browser's back and forward buttons.</p>
-
-<h2>Home component</h2>
-
-<p>
-    This sample shows how to set up the router with minimum functionality. <br/>
-    The route definition object contains a number of routes (including some with parameters and a catch-all at the end).<br/>
-    The links below allow navigating between pages.
-</p>
-
-<p>This is the Home component, which contains markup only.</p>
-
-<p><em>Hint:</em> Try navigating with the links below, then use your browser's back and forward buttons.</p>-->
