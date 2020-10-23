adb5a11b9058eef33a4171b11b433455787604fa
loris <loris.du.84@outlook.fr>
2020-10-23 17:30:37 +0200

    leaderboard

--- src/components/leaderboards/LeaderboardPage.svelte
+++ src/components/leaderboards/LeaderboardPage.svelte
@@ -1,14 +1,23 @@
-<h2>Hi there!</h2>
+<script lang="ts">
+    import Card, {Content} from '@smui/card';
+    import Button, {Group, Label} from '@smui/button';
+    import IconButton, {Icon} from '@smui/icon-button';
+    import List, {Item, Text} from '@smui/list';
 
-<p>
-    Your name is:
-    <b>{params.first}</b>
-    <b>{#if params.last}{params.last}{/if}</b>
-</p>
-<p>This comes from the URL, matching <code>/hello/:first/:last?</code>, where the last name is optional.</p>
-<p><em>Hint:</em> Try changing the URL and add your name, e.g. <code>/hello/alex</code> or <code>/hello/jane/doe</code></p>
-
-<script>
-// The params prop contains values matched from the URL
-export let params = {}
+    let clicked = 0;
 </script>
+
+<Group class="header-button mb-10" variant="raised">
+    <Button color='secondary' class="m-0" on:click={() => clicked++} variant="raised"><Label>World</Label></Button>
+    <Button color='secondary' class="m-0" on:click={() => clicked++} variant="raised"><Label>Country</Label></Button>
+    <Button color='secondary' class="m-0" on:click={() => clicked++} variant="raised"><Label>Regional</Label></Button>
+</Group>
+
+<Content component={List} class="p-0">
+    {#each [...Array(20)].map((v, i) => i + 1) as item}
+        <Item class="pb-5 pt-5" style="border-bottom: 1px solid lightgray" on:click={() => clicked++}>
+            <IconButton class="round-border material-icons m-0">face</IconButton>
+            <Text class="ml-10">Item #{item}</Text>
+        </Item>
+    {/each}
+</Content>
